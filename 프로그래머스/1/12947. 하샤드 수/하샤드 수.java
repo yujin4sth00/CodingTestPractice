668bffdd6d61c8b9f45f920f6812817891717639
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2024-09-21 18:05:25 +0900

    [level 1] Title: 하샤드 수, Time: 0.21 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum=0;
+        String[] num = Integer.toString(x).split("");
+        
+        for(int i=0;i<num.length; i++){
+            sum += Integer.parseInt(num[i]);
+        }
+        
+        if(x % sum == 0){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
+    }
+}
