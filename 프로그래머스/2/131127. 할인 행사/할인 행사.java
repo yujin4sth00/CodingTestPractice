62067d6dfe229db6b15590f598030fbf24e63954
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2024-10-22 09:07:19 +0900

    [level 2] Title: 할인 행사, Time: 99.59 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        Map<String, Integer> wMap = new HashMap<String, Integer>();
+        
+        for(int i=0; i<want.length; i++) {
+            wMap.put(want[i], number[i]);
+        }
+            
+        for(int j=0; j<discount.length - 9; j++){                 
+            int idx = 0;
+            Map<String, Integer> map = new HashMap<String, Integer>();
+            
+            for(int i=0; i<10; i++){
+                map.put(discount[i+j], map.getOrDefault(discount[i+j], 0) + 1);
+            }
+                
+            for (Map.Entry<String, Integer> entry : wMap.entrySet()) {
+                if(map.containsKey(entry.getKey()) && entry.getValue()<=map.get(entry.getKey())) {
+                    idx++;
+                }
+            }
+                    
+            if(idx == want.length) {
+                answer++;
+            }    
+        }
+        return answer;
+    }
+}
