843b7e46d144adc6e5d111d4fa4b12b85d7028eb
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2025-02-03 21:04:44 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 168 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] croatias = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String str = sc.next();
+        
+        for (int i = 0; i < croatias.length; i++) {
+            if (str.contains(croatias[i]))
+                str = str.replace(croatias[i], "@");
+        }
+        System.out.println(str.length());
+        sc.close();
+    }
+}
