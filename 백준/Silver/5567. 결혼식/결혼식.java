bbfa012a734587c4300da5358f8e46aa6331f8dc
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2025-02-22 22:28:10 +0900

    [Silver II] Title: 결혼식, Time: 156 ms, Memory: 19056 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -2,54 +2,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int cnt = 0;
-    public static boolean[] visited;
-    public static ArrayList<Integer>[] list;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int res = 0;
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
 
-    public static void Find(int depth, int start){
-        if(depth == 2){
-            return;
-        }
+		ArrayList<Integer>[] friend = new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			friend[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+            // 인덱스로 변환하기 쉽게 하기 위해 -1 해주는 코드로 수정
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			friend[a].add(b);
+			friend[b].add(a);
+		}
+         // 방문여부
+		boolean[] visited = new boolean[n];
+        // 상근이(0번 노드) 방문 처리
+		visited[0] = true;
 
-        for (int i = 0; i < list[start].size(); i++) {
-            int next = list[start].get(i);
-            visited[next] = true;
-            Find(depth + 1, next);
-        }
-    }
+        // bfs
+		Queue<Integer> q = new LinkedList<Integer>();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        visited = new boolean[n + 1];
-        list = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
-            list[i] = new ArrayList<>();
-        }
+		q.add(0); // 상근이(0번 노드)부터 탐색 시작
+		int depth= 0;  // 현재 depth
+        // bfs 탐색 시작
+		while (!q.isEmpty()) {
+			int size = q.size();  // 현재 큐에 있는 노드 수 (현재 depth에 있는 노드들)
+			for (int i = 0; i < size; i++) {
+				int x = q.poll();
+				for (int nowx : friend[x]) {
+					if (visited[nowx])
+						continue;
+					visited[nowx] = true;
+					res++;
+					q.add(nowx);
+				}
+			}
+			depth++;
+			if(depth==2) break;
+		}
 
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            list[a].add(b);
-            list[b].add(a);
-        }
-
-        visited[1] = true;
-
-        Find(0, 1);
-
-        for (int i = 2; i < visited.length; i++) {
-            if(visited[i]){
-                cnt++;
-            }
-        }
-
-        System.out.println(cnt);
-    }
+		System.out.println(res);
+	}
 }
