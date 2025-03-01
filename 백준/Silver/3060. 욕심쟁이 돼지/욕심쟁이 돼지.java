55a3afaa0c6a3588d87aecfe79622dfb4ec77332
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2025-03-02 00:15:59 +0900

    [Silver V] Title: 욕심쟁이 돼지, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.java
+++ 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.java
@@ -1,34 +1,34 @@
 import java.io.*;
-import java.util.*;
- 
+
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
- 
         StringBuilder sb = new StringBuilder();
- 
+
         int T = Integer.parseInt(in.readLine());
- 
-        for(int td = 0; td < T; td++){
+
+        for (int tc = 0; tc < T; tc++) {
             int N = Integer.parseInt(in.readLine());
-            st = new StringTokenizer(in.readLine());
- 
+
+            // 한 줄의 입력을 공백 기준으로 나누어 배열로 저장
+            String[] input = in.readLine().split(" ");
+
             int day = 1;
             long sum = 0;
- 
-            for(int i=0;i<6;i++) sum += Integer.parseInt(st.nextToken());
- 
- 
-            while(sum<=N) {
+
+            // 돼지 6마리의 초기 사료 소비량을 sum에 더함
+            for (int i = 0; i < 6; i++)
+                sum += Integer.parseInt(input[i]);
+
+            // 매일 사료량이 4배 증가, N을 초과하는 첫날 찾기
+            while (sum <= N) {
                 sum *= 4;
-                ++day;
+                day++;
             }
- 
- 
+
             sb.append(day).append("\n");
         }
- 
+
         System.out.print(sb);
     }
 }
