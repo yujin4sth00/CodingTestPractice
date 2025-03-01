7cf0482fd6b0afb464e41fec853e82f9a0bb0c72
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2025-03-02 00:25:20 +0900

    [Silver V] Title: 욕심쟁이 돼지, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.java
+++ 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.java
@@ -9,20 +9,15 @@ public class Main {
 
         for (int tc = 0; tc < T; tc++) {
             int N = Integer.parseInt(in.readLine());
-
-            // 한 줄의 입력을 공백 기준으로 나누어 배열로 저장
             String[] input = in.readLine().split(" ");
 
             int day = 1;
-            long sum = 0;
+            long firstsum = 0;
 
-            // 돼지 6마리의 초기 사료 소비량을 sum에 더함
             for (int i = 0; i < 6; i++)
-                sum += Integer.parseInt(input[i]);
-
-            // 매일 사료량이 4배 증가, N을 초과하는 첫날 찾기
-            while (sum <= N) {
-                sum *= 4;
+                firstsum += Integer.parseInt(input[i]);
+            while (firstsum <= N) {
+                firstsum *= 4;
                 day++;
             }
 
