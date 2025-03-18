ef11ac459dc55a8065847d8a43fe7282d37cc0bd
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2025-03-18 23:18:01 +0900

    [Bronze II] Title: 완전제곱수, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = m; i <= n; i++) {
+            if (Math.sqrt(i) % 1 == 0.0) {
+                sum += i;
+                min = Math.min(min, i);
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+    }
+}
