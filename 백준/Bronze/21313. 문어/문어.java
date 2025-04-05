253a313aacfc91b71de711e89956c16483b1cee2
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2025-04-05 21:41:41 +0900

    [Bronze II] Title: 문어, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/21313. 문어/문어.java
@@ -0,0 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = read(), m = n / 2;
+
+        while (m-- > 0) sb.append("1 2 ");
+        sb.append((n & 1) == 0 ? "" : "3");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
