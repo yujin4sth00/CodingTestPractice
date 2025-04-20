765e8f81a7d6829e4952cfe2c470b0199089a2ab
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2025-04-20 20:15:24 +0900

    [Silver IV] Title: 기타줄, Time: 104 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.java
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -1,28 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.Arrays;
- 
- 
+
 public class Main {
-    public static void main(String[] args) throws Exception {
- 
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N = sc.nextInt();
-        int M =sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         int Min = Integer.MAX_VALUE;
- 
+
         int[] unit = new int[M];
         int[] pack = new int[M];
-        for(int i=0; i<M; i++){
-            pack[i] = sc.nextInt();
-            unit[i] = sc.nextInt();
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            pack[i] = Integer.parseInt(st.nextToken());
+            unit[i] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(unit);
         Arrays.sort(pack);
-        
-        Min = Math.min(((N/6)+1)*pack[0], N*unit[0]);    
-        Min = Math.min(Min, ((N/6))*pack[0] + (N%6)*unit[0]);
-        
+
+        Min = Math.min(((N / 6) + 1) * pack[0], N * unit[0]);
+        Min = Math.min(Min, (N / 6) * pack[0] + (N % 6) * unit[0]);
+
         System.out.println(Min);
     }
 }
