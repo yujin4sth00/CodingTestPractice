c70af318d389f2958f53c353f1cd0481e1be678c
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2025-04-20 20:29:39 +0900

    [Silver IV] Title: 기타줄, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.java
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -1,31 +1,37 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int pack = 1000;
+		int single = 1000;
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			pack = Math.min(pack, Integer.parseInt(st.nextToken()));
+			single = Math.min(single, Integer.parseInt(st.nextToken()));
+		}
+		
+		int A, B, C;
+		
+		if(N % 6 == 0) {
+			A = pack * (N / 6);
+		}else {
+			A = pack * ((N / 6) + 1);
+		}
+		B = (pack * (N / 6)) + (single * (N % 6));
+		C = single * N;
+		
+		int result = Math.min(A, Math.min(B, C));
+		
+		System.out.println(result);
+	}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 필요한 줄 개수
-        int M = Integer.parseInt(st.nextToken()); // 브랜드 수
-
-        int minPack = Integer.MAX_VALUE;
-        int minUnit = Integer.MAX_VALUE;
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int pack = Integer.parseInt(st.nextToken());
-            int unit = Integer.parseInt(st.nextToken());
-
-            minPack = Math.min(minPack, pack);
-            minUnit = Math.min(minUnit, unit);
-        }
-
-        int result = Math.min(((N / 6) + 1) * minPack, N * minUnit); 
-        result = Math.min(result, (N / 6) * minPack + (N % 6) * minUnit); 
-        System.out.println(result);
-    }
 }
