e470691214597c00fbc5bd7a81b1bbc7622f638b
고유진 <115778958+yujin4sth00@users.noreply.github.com>
2025-05-06 15:27:53 +0900

    [Silver II] Title: 상자넣기, Time: 124 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // DP 배열 초기화
+        Arrays.fill(dp, 1);
+
+        int max = 0;
+
+        // LIS 알고리즘 적용
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        // 결과 출력
+        System.out.println(max);
+    }
+}
